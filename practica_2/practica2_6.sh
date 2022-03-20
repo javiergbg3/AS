4c2f369623ce9dc6e616629a37ee17edb1ccb0a3
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 21:51:50 +0100

    Update practica2_6.sh

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -2,17 +2,17 @@
 #815579, Gonzalez Blanco, Javier, [T], [1], [B]
 #817567, García Rodríguez, Alex, [T], [1], [B]
 num=0
-DIR=$(ls $HOME/* -d -t */ | egrep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | tail -1)
+DIR=$(ls $HOME/* -d -t | egrep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | tail -1)
 if [ -z "$DIR" ] ; then
 	mkdir "$HOME/binAAA"
 	directorio="$HOME/binAAA"
 fi
 echo "Directorio destino de copia: $DIR"
 for i in $(ls); do
-if [[ -x "$i"&& -f "$i" ]]; then
+if [[ -x "$i" && -f "$i" ]]; then
 num=$(expr $num + 1)
 cp "./$i" "$DIR/$i"
-echo "./$i ha sido copiado a $DIR/$i"
+echo "$i ha sido copiado a $DIR"
 fi
 done
 if [ $num -eq 0 ] ; then
