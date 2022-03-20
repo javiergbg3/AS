2626e26776983d84ed534978a97a59ae97009f70
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 20:29:43 +0100

    Update practica2_5.sh

--- practica_2/practica2_5.sh
+++ practica_2/practica2_5.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#815579, Gonzalez Blanco, Javier, [T], [1], [B]
+#817567, García Rodríguez, Alex, [T], [1], [B]
 echo "Introducca el nombre de un directorio: "
 read dir
 if [ -d $dir ]
