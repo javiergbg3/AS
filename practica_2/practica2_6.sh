a479fa20dea542304171cf526dbfdae10de83c62
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 21:20:34 +0100

    Update practica2_6.sh

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -8,3 +8,15 @@ if [ -z "$DIR" ] ; then
 	directorio="$HOME/binAAA"
 fi
 echo "Directorio destino de copia: $DIR"
+for i in $(ls); do
+if [[ -x "$i"&& -f "$i" ]]; then
+num=$(expr $num + 1)
+cp "./$i" "$DIR/$i"
+echo "./$i ha sido copiado a $DIR/$i"
+fi
+done
+if [ $num -eq 0 ] ; then
+echo "No se ha copiado ningun archivo"
+else
+echo "Han sido copiados $num ficheros"
+fi
