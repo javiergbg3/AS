2425e190cfb946816cab1540627641f617b80ba5
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 20:52:16 +0100

    Update practica2_6.sh

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -1,11 +1,3 @@
-
 #!/bin/bash
-
-DIR=$(ls -d -t */ | egrep bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9])
-DIR=bin013
-for i in $(ls); do
-if [[ -x "$i"&& -f "$i" ]]; then
-echo "$i"
-mv $i $DIR/
-fi
-done
+#815579, Gonzalez Blanco, Javier, [T], [1], [B]
+#817567, García Rodríguez, Alex, [T], [1], [B]
