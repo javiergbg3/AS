172faf62181c943bcdfd170c0631ccebc22fffe9
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 21:36:13 +0100

    Update practica2_6.sh

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#815579, Gonzalez Blanco, Javier, [T], [1], [B]
+#817567, García Rodríguez, Alex, [T], [1], [B]
 num=0
 DIR=$(ls $HOME/* -d -t */ | egrep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | tail -1)
 if [ -z "$DIR" ] ; then
