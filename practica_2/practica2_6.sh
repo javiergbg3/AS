40873fc798de601adbaf00c73aee2ac986f79762
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 21:56:13 +0100

    Update practica2_6.sh

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -4,7 +4,7 @@
 num=0
 DIR=$(ls $HOME/* -d -t | egrep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | tail -1)
 if [ -z "$DIR" ] ; then
-	mkdir "$HOME/binAAA"
+	mktemp "$HOME/binAAA"
 	directorio="$HOME/binAAA"
 fi
 echo "Directorio destino de copia: $DIR"
