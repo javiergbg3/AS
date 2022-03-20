8ce4c682fa3168675b1f4376ea75a0ed6ee45af4
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 22:22:24 +0100

    Update practica2_6.sh

--- practica_2/practica2_6.sh
+++ practica_2/practica2_6.sh
@@ -5,9 +5,9 @@ num=0
 DIR=$(ls $HOME/* -d -t | egrep 'bin[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | tail -1)
 if [ -z "$DIR" ] ; then
 	
-	directorio=$(mktemp -d "$HOME/binXXX")
-	echo "$directorio"
-	echo "Se ha creado el directorio $directorio"
+	DIR=$(mktemp -d "$HOME/binXXX")
+	echo "$DIR"
+	echo "Se ha creado el directorio $DIR"
 fi
 echo "Directorio destino de copia: $DIR"
 for i in $(ls); do
