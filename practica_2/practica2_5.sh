b91dca082bbbb4bfdef654461a504c42c5b4775a
javiergbg3 <79220834+javiergbg3@users.noreply.github.com>
2022-03-20 20:49:31 +0100

    Update practica2_5.sh

--- practica_2/practica2_5.sh
+++ practica_2/practica2_5.sh
@@ -4,8 +4,8 @@
 echo "Introduzca el nombre de un directorio: "
 read dir
 if [[ -d $dir  ]] ; then
-arch=$(ls "$dir" -l | grep  ^[td] | wc -l)
-dire=$(ls "$dir" -l | grep  ^[t-] | wc -l)
+arch=$(ls "$dir" -l | grep -v ^[td] | wc -l)
+dire=$(ls "$dir" -l | grep -v ^[t-] | wc -l)
 	echo "El numero de ficheros y directorios en $dir es de $arch y $dire, respectivamente"
 else 
 echo "$dir no es un directorio"
